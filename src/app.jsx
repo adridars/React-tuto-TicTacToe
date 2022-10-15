280ee7735360d5c2ca7e0d0280c3bfc3e84c7bde
Adrián Darsie <adrian_darsie@hotmail.com>
2022-10-14 21:22:07 -0300

    renaming main file

--- src/app.jsx
+++ src/app.jsx
@@ -60,21 +60,19 @@ function Board() {
   );
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+const Game = () => {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
       </div>
-    );
-  }
-}
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+};
 
 function calculateWinner(squares) {
   const lines = [
